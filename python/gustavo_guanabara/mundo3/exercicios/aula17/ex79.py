4c1b1c74cf6d420b8b55c8471ee8ecb56279ca95
croliveira <call.oliveira@hotmail.com>
2022-05-16 08:55:50 -0300

    Update exercícios

--- python/gustavo_guanabara/mundo3/exercicios/aula17/ex79.py
+++ python/gustavo_guanabara/mundo3/exercicios/aula17/ex79.py
@@ -1,4 +1,22 @@
 valores = list()
-cont = 'null'
+resp = 'null'
+maior = menor = 0
 while True:
-    valores.append(int(input('Digite um valor: ')))
+    valor = int(input('Digite um valor: '))
+    if not valor in valores:
+        valores.append(valor)
+        if valor > maior or valor == 0:
+            maior = valor
+        if valor < menor or valor == 0:
+            menor = valor
+    else:
+        print('Este valor já existe na lista e não será adicionado novamente!!!')
+    while not resp in 'SsNn':
+        resp = str(input('Deseja cadastrar novos números? [S/N] '))
+    if resp in 'Nn':
+        break
+    else:
+        resp = 'null'
+print(f'Os números digitados foram: {sorted(valores)}')
+print(f'Maior valor: {maior}')
+print(f'Menor valor: {menor}')
