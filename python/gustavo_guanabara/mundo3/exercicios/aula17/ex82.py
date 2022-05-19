b29f33cebaab0d677f3579346270295a3b6e94c5
Call Oliveira <call.oliveira@hotmail.com>
2022-05-19 16:27:18 -0300

    Update exercícios

---
+++ python/gustavo_guanabara/mundo3/exercicios/aula17/ex82.py
@@ -0,0 +1,22 @@
+valores = list()
+ivalores = list()
+pvalores = list()
+resp = 'null'
+while True:
+    valores.append(int(input('Digite um valor: ')))
+    while not resp in 'SsNn':
+        resp = str(input('Deseja continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+    resp = 'null'
+print(valores)
+
+for v in valores:
+    if v % 2 == 0:
+        pvalores.append(v)
+    else:
+        ivalores.append(v)
+        
+print(f'Valores digitados: {valores}')
+print(f'Valores ímpares: {sorted(ivalores)}')
+print(f'Valores pares: {sorted(pvalores)}')
