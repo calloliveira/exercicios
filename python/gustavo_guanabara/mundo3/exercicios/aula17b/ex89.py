be33ccbb522b2db6f4de9c4ae3a4ec5c87fc7b8f
croliveira <call.oliveira@hotmail.com>
2022-06-13 10:03:05 -0300

    Ex update

--- python/gustavo_guanabara/mundo3/exercicios/aula17b/ex89.py
+++ python/gustavo_guanabara/mundo3/exercicios/aula17b/ex89.py
@@ -1,12 +1,17 @@
+aluno = list()
 boletim = list()
 c = 0
 while True:
-    nome = str(input('Digite o nome do aluno: '))
-    nota1 = int(input('Digite a Nota 1: '))
-    nota2 = int(input('Digite a Nota 2: '))
-    boletim[c].append([nome, nota1, nota2])
+    aluno.append(str(input('Digite o nome do aluno: ')))
+    aluno.append(float(input('Digite a primeira nota: ')))
+    aluno.append(float(input('Digite a segunda nota: ')))
+    boletim.append(aluno[:])
     cont = str(input('Deseja continuar [S/N] ? '))
+    aluno.clear()
     if cont in 'Nn':
         break
-    c += 1
-print(boletim)
+print('=-' * 30)
+print('BOLETIM DA CLASSE')
+print('=-' * 30)
+for a in boletim:
+    print(f'Aluno: {a[0]} Nota1: {a[1]} Nota2: {a[2]} Média: {(a[1] + a[2]) / 2}')
