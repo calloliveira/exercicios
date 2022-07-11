862d0aae227299bc9f90b0d697c7348dcbc2c00f
croliveira <call.oliveira@hotmail.com>
2022-07-11 09:54:23 -0300

    Exercícios diários

--- python/gustavo_guanabara/mundo3/exercicios/aula19/ex91.py
+++ python/gustavo_guanabara/mundo3/exercicios/aula19/ex91.py
@@ -1,13 +1,23 @@
 from random import randint
 from time import sleep
+from operator import itemgetter
 jogadores = dict()
-ranking = dict()
-maior = 0
-for c in range(0,4):
-    jogadores[f'Jogador{c+1}'] = randint(1,5)
+ranking = list()
+sorteio = c = 0
+while len(jogadores) != 4:
+    sorteio = randint(1,6)
+    if not sorteio in jogadores.values():
+        c += 1
+        jogadores[f'Jogador{c}'] = sorteio
 print()
 print(f'{"SORTEIO":-^30}')
 for k, v in jogadores.items():
-    print(f'O {k} tirou {v}')
+    print(f'O {k} tirou {v} no dado...')
+    sleep(1)
+print('-' * 30)
+print(f'{"RANKING":-^30}')
+ranking = sorted(jogadores.items(), key=itemgetter(1), reverse=True)
+for i, v in enumerate(ranking):
+    print(f'{i+1}º: {v[0]} com {v[1]} pontos...')
     sleep(1)
 print('-' * 30)
