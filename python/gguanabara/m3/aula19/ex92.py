66aa968a6544ec0ddb3d71a37d910f988a542a17
croliveira <call.oliveira@hotmail.com>
2022-08-01 09:58:02 -0300

    Treino diário

--- python/gguanabara/m3/aula19/ex92.py
+++ python/gguanabara/m3/aula19/ex92.py
@@ -5,13 +5,13 @@ residual = 0
 funcionario["Nome"] = str(input('Digite o nome: '))
 funcionario["Nascimento"] = int(input('Digite o ano de nascimento: '))
 funcionario["Idade"] = hoje - funcionario["Nascimento"]
-funcionario["Ctps"] = int(input('Digite o Nº da CTPS: '))
+funcionario["Ctps"] = int(input('Digite o Nº da CTPS (0 não tem): '))
 if funcionario["Ctps"] != 0:
     funcionario["Ano_Contratacao"] = int(input('Qual o ano de contratação? '))
-    funcionario["Salário"] = float(input("Qual o salário atual? "))
+    funcionario["Salário"] = float(input("Qual o salário? "))
     residual = hoje - funcionario["Ano_Contratacao"]
-    if residual < 30:
-        faltante = 30 - residual
+    if residual < 35:
+        faltante = 35 - residual
         funcionario["Idade_Aposentadoria"] = funcionario["Idade"] + faltante
 print('=-' * 30)
 print(f'{"DADOS DO TRABALHADOR":=^50}')
--- python/gguanabara/m3/aula19/ex93.py
+++ python/gguanabara/m3/aula19/ex93.py
@@ -1,15 +1,26 @@
 geral = dict()
+GolsPartidas = list()
 tot = 0
 media = 0
 geral["Nome"] = str(input('Qual o nome do jogador? '))
 geral["Qtd_Partidas"] = int(input('Qual a quantidade de partidas jogadas? '))
 for c in range(0, geral["Qtd_Partidas"]):
-    geral[f"Partida {c+1}"] = int(input(f'Quantidade de gols na partida de Nº {c+1}: '))
-    tot += geral[f"Partida {c+1}"]
+    gols = int(input(f'Quantidade de gols na partida de Nº {c+1}: '))
+    GolsPartidas.append(gols)
+    tot += gols
+geral["Gols"] = GolsPartidas[:]
 geral["Total_Gols"] = tot
 geral["Media_Gols_Partida"] = tot / geral["Qtd_Partidas"]
 print('=-' * 20)
 print(f'{"APROVEITAMENTO DO JOGADOR":*^30}')
+print('=-' * 20)
+print(geral)
+print('=-' * 20)
 for k, v in geral.items():
     print(f'{k}: {v}')
+print('=-' * 20)
+print(f'O jogador {geral["Nome"]} jogou {geral["Qtd_Partidas"]} partidas.')
+for i, g in enumerate(geral["Gols"]):
+    print(f' => Na partida {i} fez {g} gol(s)')
+print(f'Fez um total de {tot} gols!!!')
 print('=-' * 20)
