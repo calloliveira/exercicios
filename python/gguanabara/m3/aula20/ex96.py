a2ac09dd4ab5b526d21cee6b12cc1f42322927df
Call Oliveira <53480613+calloliveira@users.noreply.github.com>
2022-08-08 16:52:39 -0300

    Update

--- python/gguanabara/m3/aula20/ex96.py
+++ python/gguanabara/m3/aula20/ex96.py
@@ -1,8 +1,11 @@
 def area(l, c):
     total = l * c
-    print(f'A área do total é de {total} M²...')
+    print(f'A área do total de um terreno de {l} x {c} é de {total} M²...')
 
+#Program Principal
+print('-' * 20)
+print('Cálculo de terrenos')
+print('-' * 20)
 largura = float(input('Digite a largura: '))
 comprimento = float(input('Digite o comprimento: '))
-
 area(largura, comprimento)
--- python/gguanabara/m3/aula20/ex98.py
+++ python/gguanabara/m3/aula20/ex98.py
@@ -1,31 +1,37 @@
 from time import sleep
 def contador(inicio, fim, passo):
-    cont = 0
-    print('-=' * 20)
+
     if passo == 0:
         passo = 1
     if passo < 0:
         passo = passo*-1
-    print(f'Contagem de {inicio} até {fim} de {passo} em {passo}...')
+
     cont = inicio
+
+    print('-=' * 20)
+    print(f'Contagem de {inicio} até {fim} de {passo} em {passo}...')
+
     if inicio < fim:
         while cont <= fim:
-            print(f'{cont}', end=' ')
-            cont += passo
+            print(f'{cont}', end=' ', flush=True)
+            sleep(0.1)
+            cont += passo        
     elif inicio > fim:
         while cont >= fim:
-            print(f'{cont}', end=' ')
+            print(f'{cont}', end=' ', flush=True)
+            sleep(0.1)
             cont -= passo
     print('FIM')
-    print('-=' * 20)
+
+
+#Program principal
 contador(1, 10, 1)
 contador(10, 0, 2)
+print('-=' * 20)
 print('Agora é sua vez de personalizar a contagem!!!')
 user_start = int(input('Escolha o início: '))
 user_end = int(input('Escolha o fim: '))
 user_step = str(input('Escolha o passo: '))
-if user_step == '':
-    user_step = '1'
 user_step = int(user_step)
 contador(user_start, user_end, user_step)
-
+print('-=' * 20)
